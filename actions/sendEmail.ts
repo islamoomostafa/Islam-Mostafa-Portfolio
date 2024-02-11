0a30480e22e56a78feccad3fe4a2d7ca578b70d4
Islam Mostafa <islamoomostafa@gmail.com>
2024-02-11 23:39:18 +0200

    Mail Server commit 2

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,30 +5,34 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// Check for the API key before creating the Resend instance
+const resend = process.env.RESEND_API_KEY
+  ? new Resend(process.env.RESEND_API_KEY)
+  : null; // If missing, create a null instance
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // simple server-side validation
+  // Simple server-side validation
   if (!validateString(senderEmail, 500)) {
-    return {
-      error: "Invalid sender email",
-    };
+    return { error: "Invalid sender email" };
   }
   if (!validateString(message, 5000)) {
-    return {
-      error: "Invalid message",
-    };
+    return { error: "Invalid message" };
+  }
+
+  // Check if the Resend instance is valid before using it
+  if (!resend) {
+    return { error: "Missing Resend API key" };
   }
 
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: "islamoomostafa@gmail.com",
-      subject: "Message from contact form",
+      subject: "Message from your portfolio",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
@@ -36,12 +40,8 @@ export const sendEmail = async (formData: FormData) => {
       }),
     });
   } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
-    };
+    return { error: getErrorMessage(error) };
   }
 
-  return {
-    data,
-  };
+  return { data };
 };
